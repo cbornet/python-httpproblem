b29c0f46e653b0b70d3855df28efb16a3d6e783a
cbornet <cbornet@hotmail.com>
2018-02-08 16:52:52 +0100

    Add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+from os import path
 from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
 
 setup(
     name='httpproblem',
@@ -7,5 +14,7 @@ setup(
     license='MIT',
     author='Christophe Bornet',
     author_email='cbornet@hotmail.com',
-    description='Utility library to work with RFC7807 Problem Details for HTTP APIs'
+    description='Utility library to work with RFC7807 Problem Details for HTTP APIs',
+    long_description=long_description,
+    packages=['httpproblem']
 )
